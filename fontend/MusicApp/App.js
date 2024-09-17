674f476625088cdeda10fc165d0b6de312436a2c
manhhieu1204 <vomanhhieu62@gmail.com>
2024-09-17 17:43:55 +0700

    move component PodcastIem

--- fontend/MusicApp/App.js
+++ fontend/MusicApp/App.js
@@ -1,16 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import {
-  Register,
-  RegisterEmail,
-  RegisterPassword,
-  Home,
-  Search,
-  Login,
-  Library,
-  SearchForYou,
-} from "./src/screens";
+	Register,
+	RegisterEmail,
+	RegisterPassword,
+	Home,
+	Search,
+	Login,
+	Library,
+	SearchForYou,
+} from './src/screens';
 
 export default function App() {
-  return <SearchForYou />;
+	return <Home />;
 }
--- fontend/MusicApp/components/index.js
+++ fontend/MusicApp/components/index.js
@@ -1,7 +1,8 @@
-import MusicItem from "./MusicItem";
-import Category from "./Category/Category";
-import CategoryVideo from "./Category/CategoryVideo";
-import Artists from "./Library/Artists";
-import Cards from "./ForYou/Cards";
+import MusicItem from './MusicItem';
+import Category from './Category/Category';
+import CategoryVideo from './Category/CategoryVideo';
+import Artists from './Library/Artists';
+import Cards from './ForYou/Cards';
+import PodcastItem from './PodcastItem';
 
-export { Category, CategoryVideo, MusicItem, Artists, Cards };
+export { Category, CategoryVideo, MusicItem, Artists, Cards, PodcastItem };
