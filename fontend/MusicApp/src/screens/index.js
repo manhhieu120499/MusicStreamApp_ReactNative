e6a8ae209071d50ecbf438841adb0a7e9f9ba3c7
manhhieu1204 <vomanhhieu62@gmail.com>
2024-09-17 16:16:54 +0700

    fix conflict and update dependency

--- fontend/MusicApp/src/screens/index.js
+++ fontend/MusicApp/src/screens/index.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import Register from './Register';
 import RegisterEmail from './Register/RegisterEmail';
 import RegisterPassword from './Register/RegisterPassword';
 import Home from './Home';
+import Search from './Search';
 
-export { Register, RegisterEmail, RegisterPassword, Home };
-=======
-import Register from "./Register";
-import RegisterEmail from "./Register/RegisterEmail";
-import RegisterPassword from "./Register/RegisterPassword";
-import Search from "./Search";
-
-export { Register, RegisterEmail, RegisterPassword, Search };
->>>>>>> 1812f559992dfa098de43023a966bd25453f9b14
+export { Register, RegisterEmail, RegisterPassword, Home, Search };
